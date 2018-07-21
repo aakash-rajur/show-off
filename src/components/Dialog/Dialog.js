b21c9bf30da8968386082ee7fac40533fabb88a3
Aakash Rajur <aakashrajur16@gmail.com>
2018-07-21 15:22:29 +0530

    dialog can be functional

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,32 +1,30 @@
 import PropTypes from 'prop-types'
-import React, {Component} from 'react';
+import React from 'react';
 import FadeInOut from "../FadeInOut/FadeInOut";
 
-class Dialog extends Component {
-	static propTypes = {
-		open: PropTypes.bool,
-		className: PropTypes.string,
-		duration: PropTypes.string
-	};
-	
-	static defaultProps = {
-		open: false
-	};
-	
-	render() {
-		let {
-			className,
-			children,
-			open, duration
-		} = this.props;
-		return (
-			<FadeInOut
-				className={className} duration={duration}
-				display='grid' visible={open}>
-				{children}
-			</FadeInOut>
-		)
-	}
+function Dialog(props) {
+	let {
+		className,
+		children,
+		open, duration
+	} = props;
+	return (
+		<FadeInOut
+			className={className} duration={duration}
+			display='grid' visible={open}>
+			{children}
+		</FadeInOut>
+	)
 }
+
+Dialog.propTypes = {
+	open: PropTypes.bool,
+	className: PropTypes.string,
+	duration: PropTypes.string
+};
+
+Dialog.defaultProps = {
+	open: false
+};
 
 export default Dialog;
